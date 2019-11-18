fbc9db8e964e3bed2594fb4586233544e66a2359
Machmud Effendi <machmudeffendi01@gmail.com>
2019-11-18 14:42:35 +0700

    Updates

---
+++ precache-manifest.a85b0e7d580b1ed75bafad1d9eb5f721.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17eedd547521fc33e4288a4c19e2ee24",
+    "url": "/quran-web-app/index.html"
+  },
+  {
+    "revision": "8f7ce6bf3fc38eb1e96e",
+    "url": "/quran-web-app/static/css/main.8015fefe.chunk.css"
+  },
+  {
+    "revision": "55ab46d9a683b4d8f3a3",
+    "url": "/quran-web-app/static/js/2.213f5cfb.chunk.js"
+  },
+  {
+    "revision": "8f7ce6bf3fc38eb1e96e",
+    "url": "/quran-web-app/static/js/main.22ce322b.chunk.js"
+  },
+  {
+    "revision": "657fb1bee49b4e5ec932",
+    "url": "/quran-web-app/static/js/runtime-main.b7a8f195.js"
+  }
+]);
